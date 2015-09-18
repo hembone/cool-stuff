c0c8b2ff7522bac77b57a67ab2f82bfde4603b3d
Jason Hemmy <jasonhemmy@gmail.com>
2015-09-18 11:26:38 -0500

    update to utils.js

--- agegate-js/agegate.js
+++ agegate-js/agegate.js
@@ -52,19 +52,19 @@ var AGEGATE = {
 		html += '<h1>When were you born?</h1>';
 
 		html += '<select name="month">';
-		for(i=1; i<=12; i++) { 
+		for(i=1; i<=12; i++) {
 			html += '<option value="'+i+'">'+i+'</option>';
 		}
 		html += '</select>';
 
 		html += '<select name="day">';
-		for(i=1; i<=31; i++) { 
+		for(i=1; i<=31; i++) {
 			html += '<option value="'+i+'">'+i+'</option>';
 		}
 		html += '</select>';
 
 		html += '<select name="year">';
-		for(i=2015; i>=1920; i--) { 
+		for(i=2015; i>=1920; i--) {
 			html += '<option value="'+i+'">'+i+'</option>';
 		}
 		html += '</select>';
@@ -84,7 +84,7 @@ var AGEGATE = {
 
 	,cookie : {
 		set : function(name, value, days) {
-			if (days) {
+			if(days) {
 				var date = new Date();
 				date.setTime(date.getTime()+(days*24*60*60*1000));
 				var expires = "; expires="+date.toGMTString();
@@ -94,10 +94,10 @@ var AGEGATE = {
 		,get : function(name) {
 			var nameEQ = name + "=";
 			var ca = document.cookie.split(';');
-			for (var i=0;i < ca.length;i++) {
+			for(var i=0;i < ca.length;i++) {
 				var c = ca[i];
 				while (c.charAt(0)==' ') c = c.substring(1,c.length);
-				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+				if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 			}
 			return null;
 		}
--- utils-js/utils.js
+++ utils-js/utils.js
@@ -2,39 +2,63 @@ var UTILS = {
 
 	cookie : {
 		set : function(name, value, days) {
-			if (days) {
+			var expires = '';
+			if(days) {
 				var date = new Date();
 				date.setTime(date.getTime()+(days*24*60*60*1000));
-				var expires = "; expires="+date.toGMTString();
-			} else var expires = "";
+				expires = "; expires="+date.toGMTString();
+			} else {
+				expires = '';
+			}
 			document.cookie = name+"="+value+expires+"; path=/";
-		}
-		,get : function(name) {
+		},
+		get : function(name) {
 			var nameEQ = name + "=";
 			var ca = document.cookie.split(';');
-			for (var i=0;i < ca.length;i++) {
+			for(var i=0;i < ca.length;i++) {
 				var c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1,c.length);
-				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+				while(c.charAt(0)===' ') {
+					c = c.substring(1,c.length);
+				}
+				if(c.indexOf(nameEQ) === 0) {
+					return c.substring(nameEQ.length,c.length);
+				}
 			}
 			return null;
-		}
-		,remove : function(name) {
+		},
+		remove : function(name) {
 			UTILS.cookie.set(name,"",-1);
 		}
-	}
+	},
 
-	,popupWindow : function(url, title, w, h) {
+	popupWindow : function(url, title, w, h) {
 		var left = (screen.width/2)-(w/2);
 		var top = (screen.height/2)-(h/2);
 		return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-	}
+	},
 
 	// requires jQuery
-	,animateScrollTo : function(offset, speed) {
-		$('html, body').animate({ 
+	animateScrollTo : function(offset, speed) {
+		$('html, body').animate({
 			scrollTop: offset
 		}, speed);
+	},
+
+	// requires jQuery
+	resize : {
+		init : function(callback) {
+			if(typeof callback === 'function') {
+				UTILS.resize.resizeTimeout='';
+				UTILS.resize.set(callback);
+				callback();
+			}
+		},
+		set : function(callback) {
+			$(window).on('resize', function() {
+				clearTimeout(UTILS.resize.resizeTimeout);
+				UTILS.resize.resizeTimeout = setTimeout(callback, 100);
+			});
+		}
 	}
 
-}
+};
