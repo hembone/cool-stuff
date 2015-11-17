5597dcc579f7ac6212df9524d4f03e082d3f2c22
Jason Hemmy <jasonhemmy@gmail.com>
2015-11-17 08:55:46 -0600

    update

--- fcb_dev_tools/site/pages/download-center.php
+++ fcb_dev_tools/site/pages/download-center.php
@@ -3,26 +3,34 @@ $this->title='Downloads';
 $AUTH = new authHelper();
 ?>
 
-<?php if($AUTH->allowedTypes('pm')): ?>
-    Project manager
-<?php endif; ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
 
-<?php if($AUTH->allowedTypes('create')): ?>
-    Creative
-<?php endif; ?>
+        <?php if($AUTH->allowedTypes('pm')): ?>
+            Project manager
+        <?php endif; ?>
 
-<?php if($AUTH->allowedTypes('dm')): ?>
-    Development Manager
-<?php endif; ?>
+        <?php if($AUTH->allowedTypes('create')): ?>
+            Creative
+        <?php endif; ?>
 
-<?php if($AUTH->allowedTypes('dev')): ?>
-    Developer
-<?php endif; ?>
+        <?php if($AUTH->allowedTypes('dm')): ?>
+            Development Manager
+        <?php endif; ?>
 
-<?php if($AUTH->allowedTypes('admin')): ?>
-    <a href="[download me]" target="_blank">Item 1</a></br>
-    <a href="[download me]" target="_blank">Item 2</a></br>
-    <a href="[download me]" target="_blank">Item 3</a></br>
-    <a href="[download me]" target="_blank">Item 4</a></br>
-    <a href="[download me]" target="_blank">Item 5</a></br>
-<?php endif; ?>
+        <?php if($AUTH->allowedTypes('dev')): ?>
+            Developer
+        <?php endif; ?>
+
+        <?php if($AUTH->allowedTypes('admin')): ?>
+            <a href="[download me]" target="_blank">Item 1</a></br>
+            <a href="[download me]" target="_blank">Item 2</a></br>
+            <a href="[download me]" target="_blank">Item 3</a></br>
+            <a href="[download me]" target="_blank">Item 4</a></br>
+            <a href="[download me]" target="_blank">Item 5</a></br>
+        <?php endif; ?>
+
+        </div>
+    </div>
+</div>
